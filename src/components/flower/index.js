2a70dca958719750269b4cd944cf8e64b2f52bdd
nhwa <44343908+nhwa@users.noreply.github.com>
2023-03-03 23:59:19 +0900

    Design: 메인(home)화면 구성'
    - hover시 특정 영역만 보이고 click시 전체화면 보이는 화면 구성
    - glitch 효과, box geometry 구성

--- src/components/flower/index.js
+++ src/components/flower/index.js
@@ -2,21 +2,21 @@
 import React, { useRef, useState } from 'react'
 import { useFrame } from '@react-three/fiber'
 
-const Box = (props) => {
-    const ref = useRef();
-  
-    useFrame(() => {
-      ref.current.rotation.x += 0.01;
-      ref.current.rotation.y += 0.01;
-    }, []);
-  
-    return (
-        <mesh rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25} ref={ref} {...props}>
-        <boxBufferGeometry args={[70, 70, 70, 70]}/>
-        <meshLambertMaterial color={0x9178e6} />
-      </mesh>
-    );
-
-  };
-  
+const Box = () => {
+  const mesh = useRef(null)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // useFrame((state, delta) => (mesh.current.rotation.x += delta))
+  return (
+    <mesh
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[100, 100, 100]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+};
   export default Box;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,40 +1,82 @@
-import React from "react";
-import "./style.css";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-// import Typewriter from "typewriter-effect";
-import { introdata, meta } from "../../content_option";
-import { Link } from "react-router-dom";
-import  Box from "../../components/flower";
-import { OrbitControls, OrthographicCamera } from "@react-three/drei";
-import { Canvas } from '@react-three/fiber'
+  import React,{ useEffect, useRef, useState } from "react";
+  import "./style.css";
+  import { Helmet, HelmetProvider } from "react-helmet-async";
+  // import Typewriter from "typewriter-effect";
+  import { introdata, meta } from "../../content_option";
+  import { Link } from "react-router-dom";
+  import  Box from "../../components/flower";
+  import { EffectComposer, DepthOfField ,
+    Noise} from '@react-three/postprocessing';
+  import { BlendFunction } from "postprocessing";
+  import { OrbitControls, OrthographicCamera} from "@react-three/drei";
+  import { Canvas } from '@react-three/fiber'
+  import { Container } from "react-bootstrap";
+
+const Effects = () =>{
+  return (
+    <EffectComposer>
+      <DepthOfField
+        focusDistance={0}
+        focalLength={0.06}
+        bokehScale={5}
+        height={600}
+        blendFunction={BlendFunction.Screen}
+        blur={true}
+      />
+      <Noise opacity={0.25} />
+    </EffectComposer>
+)};
 
 
-export const Home = () => {
+export const Home = (event) => {
+  const magicRef = useRef(null);
+  const [isClicked,setisClicked] =useState(0);
+  let [coords, setCoords] = useState({x: 0, y: 0});
+
+
+  useEffect(() => {
+    const handleWindowMouseMove = event => {
+      setCoords({ x: event.clientX - magicRef.current.offsetWidth / 2, y: event.clientY - magicRef.current.offsetWidth / 2});
+    };
+    window.addEventListener('mousemove', handleWindowMouseMove);
+    
+    console.log("width", magicRef.current.offsetWidth)
+    console.log("xy", coords);
+    return () => {
+      window.removeEventListener( 'mousemove', handleWindowMouseMove,);
+    };
+  }, []);
+
     return (
-      <Canvas style={{ position:"absolute", width: "100vw", height: "100vh", top:"0", overflow:"hidden"}}>
-        <ambientLight intensity={1} />
-        <pointLight intensity={0.75} position={[10, 10, 10]} />
-        {/* <Box position={[200, 200, 0]} /> */}
-        <Box position={[0, 105, 0]} />
-        <Box position={[105, 70, 0]} />
-        <Box position={[105, -45, 0]} />
-        <Box position={[0, 0, 0]} />
-        <Box position={[-105, 70, 0]} />
-        <Box position={[-105, -45, 0]} />
-        <Box position={[0, -105, 0]} />
-        {/* <OrbitControls /> */}
-        <OrthographicCamera
-          makeDefault
-          zoom={0.5}
-          top={200}
-          bottom={-200}
-          left={300}
-          right={-300}
-          near={1}
-          far={1500}
-          position={[0, 0, 200]}
-        />
-    </Canvas>
+    <>
+      <div className="scene" onClick= {() =>{setisClicked(1)}}>
+        <h1>Simple magic!</h1>
+        { isClicked 
+        ? <div className="magic" ref={magicRef} style={ {display :'none' }}></div>
+        : <div className="scene_wrapper"><div className="magic" ref={magicRef} style={ { left: coords.x, top :coords.y}}></div></div> }
+        <p className="check-out">Check out my other <a href="https://codepen.io/suez/public/" target="_blank">pens</a></p>
+      </div>
+      </>    
+      // <Canvas></Canvas>
+    //   <Canvas style={{ position:"absolute", width: "100vw", height: "100vh", top:"0", overflow:"hidden"}}>
+    //     <ambientLight intensity={1} />
+    //     <pointLight intensity={0.75} position={[10, 10, 10]} />
+    //     <Box position={[-105, -45, 0]} />
+    //     {/* <OrbitControls /> */}
+    //     {/* <Effects /> */}
+        
+    //     <OrthographicCamera
+    //       makeDefault
+    //       zoom={1}
+    //       top={200}
+    //       bottom={-200}
+    //       left={300}
+    //       right={-300}
+    //       near={1}
+    //       far={1500}
+    //       position={[0, 0, 200]}
+    //     />
+    // </Canvas>
     );
   };
 
