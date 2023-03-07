794945b5f7ed5a06602f004b16fe10df6a8e203f
nhwa <44343908+nhwa@users.noreply.github.com>
2023-03-08 01:48:41 +0900

    Style: '필요없는 코드 삭제'

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,6 @@
   import { Helmet, HelmetProvider } from "react-helmet-async";
   // import Typewriter from "typewriter-effect";
   import { introdata, meta } from "../../content_option";
-  import { Link } from "react-router-dom";
   import  Box from "../../components/flower";
   import { EffectComposer, DepthOfField ,
     Noise} from '@react-three/postprocessing';
@@ -12,22 +11,6 @@
   import { Canvas } from '@react-three/fiber'
   import { Container } from "react-bootstrap";
 
-const Effects = () =>{
-  return (
-    <EffectComposer>
-      <DepthOfField
-        focusDistance={0}
-        focalLength={0.06}
-        bokehScale={5}
-        height={600}
-        blendFunction={BlendFunction.Screen}
-        blur={true}
-      />
-      <Noise opacity={0.25} />
-    </EffectComposer>
-)};
-
-
 export const Home = (event) => {
   const magicRef = useRef(null);
   const [isClicked,setisClicked] =useState(0);
@@ -35,48 +18,24 @@ export const Home = (event) => {
 
 
   useEffect(() => {
-    const handleWindowMouseMove = event => {
-      setCoords({ x: event.clientX - magicRef.current.offsetWidth / 2, y: event.clientY - magicRef.current.offsetWidth / 2});
+    const handleWindowMouseMove = (e) => {
+      setCoords({ x: e.clientX - magicRef.current.offsetWidth / 2, y: e.clientY - magicRef.current.offsetWidth / 2});
     };
     window.addEventListener('mousemove', handleWindowMouseMove);
-    
-    console.log("width", magicRef.current.offsetWidth)
-    console.log("xy", coords);
     return () => {
       window.removeEventListener( 'mousemove', handleWindowMouseMove,);
     };
   }, []);
 
     return (
-    <>
       <div className="scene" onClick= {() =>{setisClicked(1)}}>
-        <h1>Simple magic!</h1>
-        { isClicked 
-        ? <div className="magic" ref={magicRef} style={ {display :'none' }}></div>
-        : <div className="scene_wrapper"><div className="magic" ref={magicRef} style={ { left: coords.x, top :coords.y}}></div></div> }
-        <p className="check-out">Check out my other <a href="https://codepen.io/suez/public/" target="_blank">pens</a></p>
+        <h1>Find your Color</h1>
+        { 
+          isClicked 
+          ? <div className="magic" ref={magicRef} style={ {display :'none' }}></div>
+          : <div className="scene_wrapper"><div className="magic" ref={magicRef} style={ { left: coords.x, top :coords.y}}></div></div> 
+        }
       </div>
-      </>    
-      // <Canvas></Canvas>
-    //   <Canvas style={{ position:"absolute", width: "100vw", height: "100vh", top:"0", overflow:"hidden"}}>
-    //     <ambientLight intensity={1} />
-    //     <pointLight intensity={0.75} position={[10, 10, 10]} />
-    //     <Box position={[-105, -45, 0]} />
-    //     {/* <OrbitControls /> */}
-    //     {/* <Effects /> */}
-        
-    //     <OrthographicCamera
-    //       makeDefault
-    //       zoom={1}
-    //       top={200}
-    //       bottom={-200}
-    //       left={300}
-    //       right={-300}
-    //       near={1}
-    //       far={1500}
-    //       position={[0, 0, 200]}
-    //     />
-    // </Canvas>
     );
   };
 
