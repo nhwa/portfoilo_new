f947218b2961b69aceea11a65e198e0712af6ac9
nhwa <44343908+nhwa@users.noreply.github.com>
2023-03-11 14:09:19 +0900

    Feat: 'Home Component Page 수정'
    - card template
    - item hover시 여러개의 레이어 animation 추가
    - item click시 portfolio component page로 이동
    - scroll animation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,80 @@
   import React,{ useEffect, useRef, useState } from "react";
+  import { dataportfolio } from "../../content_option";
+  import { Link } from "react-router-dom";
+  import { Portfolio } from "../portfolio";
   import "./style.css";
-  import { Helmet, HelmetProvider } from "react-helmet-async";
-  // import Typewriter from "typewriter-effect";
-  import { introdata, meta } from "../../content_option";
-  import  Box from "../../components/flower";
-  import { EffectComposer, DepthOfField ,
-    Noise} from '@react-three/postprocessing';
-  import { BlendFunction } from "postprocessing";
-  import { OrbitControls, OrthographicCamera} from "@react-three/drei";
-  import { Canvas } from '@react-three/fiber'
-  import { Container } from "react-bootstrap";
-
-export const Home = (event) => {
-  const magicRef = useRef(null);
-  const [isClicked,setisClicked] =useState(0);
-  let [coords, setCoords] = useState({x: 0, y: 0});
 
 
-  useEffect(() => {
-    const handleWindowMouseMove = (e) => {
-      setCoords({ x: e.clientX - magicRef.current.offsetWidth / 2, y: e.clientY - magicRef.current.offsetWidth / 2});
-    };
-    window.addEventListener('mousemove', handleWindowMouseMove);
-    return () => {
-      window.removeEventListener( 'mousemove', handleWindowMouseMove,);
-    };
-  }, []);
+export const Home = React.memo(() => {
+const holewrapperRef = useRef(null);
+const [coords, setCoords] = useState({x: 0, y: 0});
+const [scrollPosition, setScrollPosition] =useState(null);
 
-    return (
-      <div className="scene" onClick= {() =>{setisClicked(1)}}>
-        <h1>Find your Color</h1>
-        { 
-          isClicked 
-          ? <div className="magic" ref={magicRef} style={ {display :'none' }}></div>
-          : <div className="scene_wrapper"><div className="magic" ref={magicRef} style={ { left: coords.x, top :coords.y}}></div></div> 
-        }
-      </div>
-    );
-  };
+const mainRef = useRef();
 
+// const scrollToElement = () => {
+//   window.scrollTo({
+//       top: elementRef.current.offsetTop - 110,
+//       behavior: 'smooth'
+//   })
+// }
+// const setCursorPosition = (e) => setCoords({x: e.clientX - holewrapperRef.current.offsetWidth / 2, y: e.clientY - holewrapperRef.current.offsetWidth / 2});
+const setScrollGrid = (e) =>  setScrollPosition(((window.pageYOffset / (mainRef.current.offsetHeight - document.body.offsetHeight)) * -100) + "%");
+
+useEffect(() => {
+  document.addEventListener('scroll', (e)=> mainRef.current && setScrollGrid());
+  // window.addEventListener('mousemove', (e) => setCursorPosition(e));
+  return ()=>{
+    // window.removeEventListener('mousemove', (e)=> setCursorPosition(e));
+    document.removeEventListener('scroll', (e)=> setScrollGrid());
+  }
+}, [])
+
+  return (
+  // 25개는 안넘을듯!
+  <div className="main_section" ref={mainRef} >
+    <div className="cards" style={{'--scroll': scrollPosition}}>
+      { dataportfolio.map(data =>{
+        return(
+          <Link key={data.id} className="stack" to={"/portfolio"}  >
+            <div className="card top" >
+              <div className="items">
+                <div className="items_info">
+                  <img src={process.env.PUBLIC_URL + '/imgs/layout/'+data.icon+'_icon.png' } alt={data.icon+'_icon.png'} />
+                  <h2>
+                    <strong>{ data.icon } Project</strong> 
+                  </h2>
+                  <h3>{ data.name }</h3>
+                </div>
+                <img className="item_img" src={process.env.PUBLIC_URL + '/imgs/contents/'+ data.dir+'/'+data.card_img.src } alt={data.card_img.alt} />
+              </div>
+            </div>
+            <div className="card mid-top">
+              <div className="items">
+                <div className="item">
+                  <img className="item_img" key= {data.id} src= {process.env.PUBLIC_URL + '/imgs/contents/'+ data.dir+'/'+data.card_img.src } alt={data.card_img.alt} />
+                </div>
+              </div>
+            </div>
+            <div className="card mid-bottom">
+              <div className="items">
+              <div className="item">
+                  <img className="item_img" key= {data.id} src= {process.env.PUBLIC_URL + '/imgs/contents/'+ data.dir+'/'+data.card_img.src } alt={data.card_img.alt} />
+                </div>
+              </div>
+            </div>
+            <div className="card bottom">
+              <div className="items">
+              <div className="item">
+                  <img className="item_img" key= {data.id} src= {process.env.PUBLIC_URL + '/imgs/contents/'+ data.dir+'/'+data.card_img.src } alt={data.card_img.alt} />
+                </div>
+              </div>
+            </div>
+            <div className="card shadow"></div>
+          </Link>
+        )
+      })}
+    </div>
+  </div>
+  )
+});
